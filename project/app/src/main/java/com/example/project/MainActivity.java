88fa63f71ebb4ef9019a66e9e5bc8ab986391374
Ishmamur Farhan <farha11a@uwindsor.ca>
2022-04-11 23:56:09 -0400

    Added registration save option

--- project/app/src/main/java/com/example/project/MainActivity.java
+++ project/app/src/main/java/com/example/project/MainActivity.java
@@ -2,13 +2,39 @@ package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView des1;
+    TextView des2;
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        des1 = findViewById(R.id.des1);
+        des2 = findViewById(R.id.des2);
+        btn = findViewById(R.id.btn);
+
+        des1.setText("-> The GPA calculator is used to calculate grade point average (GPA) and generate a GPA report.\n\n-> Enter semester number, course code, name & mark\n\n-> Get GPA for each semester");
+        des2.setText("-> The Conversion Calculator is used to convert between commonly used units.");
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //This is used for starting the second activity
+                Intent i = new Intent(MainActivity.this, register.class);
+                startActivity(i);
+            }
+        });
+
     }
 }
--- project/app/src/main/java/com/example/project/register.java
+++ project/app/src/main/java/com/example/project/register.java
@@ -2,21 +2,39 @@ package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.Toast;
 
 public class register extends AppCompatActivity {
 
+    EditText firstName;
+    EditText lastName;
     Button cont;
+    Button back;
+    Switch switch_remember;
+    SharedPreferences sharedPreferences;
+    String first, last;
+    Boolean isRemember;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        firstName = findViewById(R.id.firstName);
+        lastName = findViewById(R.id.lastName);
+        switch_remember = findViewById(R.id.switch_remember);
         cont = findViewById(R.id.button_continue);
+        back = findViewById(R.id.back);
+
+        getValues();
 
         cont.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,5 +43,46 @@ public class register extends AppCompatActivity {
             }
         });
 
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(register.this, MainActivity.class));
+            }
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        saveValues();
+    }
+
+    protected  void saveValues(){
+
+        sharedPreferences = getSharedPreferences("save", Context.MODE_PRIVATE);
+        first = firstName.getText().toString();
+        last = lastName.getText().toString();
+        isRemember = switch_remember.isChecked();
+        if(isRemember){
+
+            SharedPreferences.Editor sp_editor = sharedPreferences.edit();
+            sp_editor.putString("first", first);
+            sp_editor.putString("last", last);
+            sp_editor.putBoolean("rem", isRemember);
+            sp_editor.commit();
+            Toast.makeText(getApplicationContext(), "Name saved!", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    protected void getValues(){
+        sharedPreferences = getSharedPreferences("save", Context.MODE_PRIVATE);
+
+        first = sharedPreferences.getString("first", null);
+        last = sharedPreferences.getString("last", null);
+        isRemember = sharedPreferences.getBoolean("rem", false);
+
+        firstName.setText(first);
+        lastName.setText(last);
+        switch_remember.setChecked(isRemember);
     }
 }
