ec67af4b17513604283f378cf34ce53a38e7c46a
Sawyer King <king123@uwindsor.ca>
2022-04-11 22:58:08 -0400

    Added Activities
    Added the two calculator activities and set up the functionality of the menu to bring to you to said activities.

--- project/app/src/main/java/com/example/project/menu.java
+++ project/app/src/main/java/com/example/project/menu.java
@@ -2,13 +2,36 @@ package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class menu extends AppCompatActivity {
+
+    Button gpa, conversion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        gpa = findViewById(R.id.button_gpa);
+        conversion = findViewById(R.id.button_conversion);
+
+        gpa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(menu.this, gpacalculator.class));
+            }
+        });
+
+        conversion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(menu.this, conversioncalculator.class));
+            }
+        });
+
     }
 }
