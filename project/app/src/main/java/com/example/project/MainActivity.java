a2da4e4ef03f982731fa41734ff22cbc9c004e99
Ishmamur Farhan <farha11a@uwindsor.ca>
2022-04-12 01:02:41 -0400

    Adjust the layout for menu

--- project/app/src/main/java/com/example/project/MainActivity.java
+++ project/app/src/main/java/com/example/project/MainActivity.java
@@ -23,8 +23,8 @@ public class MainActivity extends AppCompatActivity {
         des2 = findViewById(R.id.des2);
         btn = findViewById(R.id.btn);
 
-        des1.setText("-> The GPA calculator is used to calculate grade point average (GPA) and generate a GPA report.\n\n-> Enter semester number, course code, name & mark\n\n-> Get GPA for each semester");
-        des2.setText("-> The Conversion Calculator is used to convert between commonly used units.");
+        des1.setText("-> The GPA calculator is used to calculate grade point average (GPA) and generate a GPA report.\n\n-> Enter semester number, course code, name & mark.\n\n-> Get GPA for each semester.");
+        des2.setText("-> The Conversion Calculator is used to convert between commonly used units.\n\n-> Select the type of converter and add the amount.\n\n-> Get the converted value.");
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
