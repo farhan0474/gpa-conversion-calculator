a9e74789c1eb195eebb9d8435b18c9d7a4917e69
Sawyer King <king123@uwindsor.ca>
2022-04-12 21:16:17 -0400

    Added Conversion Functionality
    Created the functionality for the conversion calculator.

--- project/app/src/main/java/com/example/project/conversioncalculator.java
+++ project/app/src/main/java/com/example/project/conversioncalculator.java
@@ -8,24 +8,56 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class conversioncalculator extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
-    Button back;
+    EditText input;
+    TextView output;
+    Button back, convert;
+    String selectedConversion = "Meter to Kilometer";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_conversioncalculator);
 
+        input = findViewById(R.id.amount);
+        output = findViewById(R.id.result);
         back = findViewById(R.id.button6);
+        convert = findViewById(R.id.button_convert);
 
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(conversioncalculator.this, menu.class));
+            }
+        });
+
+        convert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (selectedConversion.equals("Meter to Kilometer")) {
+                    output.setText(""+((Double.parseDouble(input.getText().toString()))/1000));
+                } else if (selectedConversion.equals("Kilometer to Meter")) {
+                    output.setText(""+((Double.parseDouble(input.getText().toString()))*1000));
+                } else if (selectedConversion.equals("Kilogram to Pound")) {
+                    output.setText(""+((Double.parseDouble(input.getText().toString()))*2.20462262185));
+                } else if (selectedConversion.equals("Pound to Kilogram")) {
+                    output.setText(""+((Double.parseDouble(input.getText().toString()))/2.20462262185));
+                } else if (selectedConversion.equals("Pound to Ounce")) {
+                    output.setText(""+((Double.parseDouble(input.getText().toString()))*16));
+                } else if (selectedConversion.equals("Ounce to Pound")) {
+                    output.setText(""+((Double.parseDouble(input.getText().toString()))/16));
+                } else if (selectedConversion.equals("Fahrenheit to Celsius")) {
+                    output.setText(""+(((Double.parseDouble(input.getText().toString()))-32)*(5.0/9.0)));
+                } else if (selectedConversion.equals("Celsius to Fahrenheit")) {
+                    output.setText(""+(((Double.parseDouble(input.getText().toString()))/(5.0/9.0))+32));
+                }
+
             }
         });
 
@@ -38,9 +70,10 @@ public class conversioncalculator extends AppCompatActivity implements AdapterVi
 
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-        String selectedConversion = adapterView.getItemAtPosition(i).toString();
-        selectedConversion = "You have selected " + selectedConversion + "!";
-        Toast.makeText(this, selectedConversion, Toast.LENGTH_SHORT).show();
+        selectedConversion = adapterView.getItemAtPosition(i).toString();
+        String selected = "You have selected " + selectedConversion + "!";
+        Toast.makeText(this, selected, Toast.LENGTH_SHORT).show();
+        output.setText("");
     }
 
     @Override
